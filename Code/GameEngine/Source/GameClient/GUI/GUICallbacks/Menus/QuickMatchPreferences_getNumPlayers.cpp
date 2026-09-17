2778b6956c09d9c5c405b034dea5a6e0584f104d
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-17 07:24:36 +0000

    Recover QuickMatchPreferences getNumPlayers (0x5DF69E, 80B) via ZH
    ZH verbatim int getter. Same TU-local map plumbing as the
    OptionPreferences family.

---
+++ Code/GameEngine/Source/GameClient/GUI/GUICallbacks/Menus/QuickMatchPreferences_getNumPlayers.cpp
@@ -0,0 +1,77 @@
+// cl: /O1 /GX- /DNDEBUG /MD /D_STLP_USE_STATIC_LIB
+// stlport
+//
+// ?getNumPlayers@QuickMatchPreferences@@QAEHXZ, retail 0x005DF69E, 80 bytes.
+// Dedicated TU.
+//
+// ZH QuickMatchPreferences::getNumPlayers shape (miss default 0, same as ZH).
+// Same string plumbing as the OptionPreferences
+// family: TU-local StringBase/AsciiString, real STLport map (find worker
+// pinned), StringBase ctor and releaseBuffer pinned, atoi via import.
+
+#include <map>
+#include <stdlib.h>
+
+typedef bool Bool;
+typedef int Int;
+
+template <typename T> struct BfmeStringData
+{
+	int refCount;
+	unsigned short length;
+	unsigned short capacity;
+	T text[1];
+};
+
+template <typename T> class StringBase
+{
+	friend class AsciiString;
+private:
+	StringBase(const T *text);
+	StringBase(const StringBase<T> &other);
+	~StringBase() { releaseBuffer(); }
+	void releaseBuffer();
+	BfmeStringData<T> *m_data;
+
+public:
+	const T *str() const { return m_data ? &m_data->text[0] : (const T *)""; }
+};
+
+class AsciiString : private StringBase<char>
+{
+public:
+	AsciiString(const char *text) : StringBase<char>(text) {}
+	AsciiString(const AsciiString &other);
+	~AsciiString() {}
+};
+
+bool operator<(const AsciiString &left, const AsciiString &right);
+
+namespace _STL
+{
+template <> struct less<AsciiString>
+{
+	bool operator()(const AsciiString &left, const AsciiString &right) const
+	{
+		return left < right;
+	}
+};
+}
+
+typedef _STL::map<AsciiString, AsciiString> AsciiPreferenceMap;
+
+class QuickMatchPreferences : public AsciiPreferenceMap
+{
+public:
+	virtual ~QuickMatchPreferences();
+	Int getNumPlayers();
+};
+
+Int QuickMatchPreferences::getNumPlayers(void)
+{
+	QuickMatchPreferences::const_iterator it = find("NumPlayers");
+	if (it == end()) {
+		return 0;
+	}
+	return atoi(it->second.str());
+}
