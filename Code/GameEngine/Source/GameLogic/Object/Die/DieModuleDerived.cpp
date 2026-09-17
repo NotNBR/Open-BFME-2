bc763360326ed931cfcb33060c331d67729fa26c
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-17 03:21:55 +0000

    Recover opaque MI derived dtor Rva00486C76 (0x486C76, 25B) tail-jmp DieModule

--- Code/GameEngine/Source/GameLogic/Object/Die/DieModuleDerived.cpp
+++ Code/GameEngine/Source/GameLogic/Object/Die/DieModuleDerived.cpp
@@ -168,3 +168,19 @@ public:
 Rva00486B56::~Rva00486B56()
 {
 }
+
+class Rva00486C76_B2
+{
+public:
+	virtual void f2();
+};
+
+class Rva00486C76 : public DieModule, public MiBase1, public Rva00486C76_B2
+{
+public:
+	virtual ~Rva00486C76();
+};
+
+Rva00486C76::~Rva00486C76()
+{
+}
