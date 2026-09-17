e4a189c61d78f76b01387e2062e6f0a8e9f3594b
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-17 09:59:55 +0000

    Recover PointDefenseLaserUpdate ctor (0x4C9598, 43B)

---
+++ Code/GameEngine/Source/GameLogic/Object/Update/PointDefenseLaserUpdateCtor.cpp
@@ -0,0 +1,54 @@
+// cl: /O1 /MD /DNDEBUG
+// Trial: ??0PointDefenseLaserUpdate@@QAE@PAVThing@@PBVModuleData@@@Z.
+// Base 0x00362EC7 (pinned opaque); and-zeroed slot, hand-placed vtable
+// (novtable), then a global-pointer member load. Factory stub order.
+class Thing;
+class ModuleData;
+
+extern "C" char PointDefenseLaserUpdate_vftable;
+
+struct OpaqueGlobal
+{
+	char m_pad[0x40];
+	void *m_p40;
+};
+
+extern OpaqueGlobal *g_updateGlobal;
+
+// Opaque intermediate; ctor resolves to the opaque pin at 0x00362EC7.
+class Rva00362EC7
+{
+public:
+	Rva00362EC7(Thing *thing, const ModuleData *moduleData);
+	virtual ~Rva00362EC7();
+
+protected:
+	unsigned char m_pad0[8];
+	int m_i0C;
+	const void *m_p10;
+	unsigned char m_pad1[0x20 - 0x14];
+};
+
+Rva00362EC7::~Rva00362EC7()
+{
+}
+
+class __declspec(novtable) PointDefenseLaserUpdate : public Rva00362EC7
+{
+public:
+	PointDefenseLaserUpdate(Thing *thing, const ModuleData *moduleData);
+	virtual ~PointDefenseLaserUpdate();
+
+};
+
+PointDefenseLaserUpdate::PointDefenseLaserUpdate(Thing *thing, const ModuleData *moduleData)
+	: Rva00362EC7(thing, moduleData)
+{
+	m_i0C = 0;
+	*reinterpret_cast<char **>(this) = &PointDefenseLaserUpdate_vftable;
+	m_p10 = g_updateGlobal->m_p40;
+}
+
+PointDefenseLaserUpdate::~PointDefenseLaserUpdate()
+{
+}
