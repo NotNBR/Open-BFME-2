d083a4c79fd86ef13b726b8b8fb9f1e09631d83f
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-17 18:50:53 +0000

    Recover Rva004E18A2 scalar deleting dtor (0x4E1D16, 28B) via opaque dtor pin

--- Code/GameEngine/Source/Common/OpaqueScalarDeletingDtors.cpp
+++ Code/GameEngine/Source/Common/OpaqueScalarDeletingDtors.cpp
@@ -2252,6 +2252,27 @@ void Rva002CEC0A_Anchor(Rva002CEC0A *p)
 	p->Rva002CEC0A::~Rva002CEC0A();
 }
 
+class Rva004E18A2_B2
+{
+public:
+	virtual void f2();
+};
+
+class Rva004E18A2 : public Rva0049B47C, public MiBase1, public Rva004E18A2_B2
+{
+public:
+	virtual ~Rva004E18A2()
+	{
+	}
+};
+
+// Anchor: forces out-of-line emission of the in-class destructor COMDAT,
+// including the scalar deleting destructor.
+void Rva004E18A2_Anchor(Rva004E18A2 *p)
+{
+	p->Rva004E18A2::~Rva004E18A2();
+}
+
 class Rva005C3F02_B2
 {
 public:
