35a086775b454928bd4560284414b2a833db81a6
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-17 12:30:31 +0000

    Recover OBBoxRenderObjClass Clone (0x1763D0, 78B) via AABox Clone twin with global new; dedicated TU

---
+++ Code/Libraries/Source/WWVegas/WW3D2/OBBoxRenderObjClone.cpp
@@ -0,0 +1,12 @@
+// cl: /Ireference/shims/bfme2renderobj /DNDEBUG /MD /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWMath /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWLib /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWSaveLoad /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WW3D2 /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/Wwutil /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWDownload /Ireference/open-bfme-1/Code/Libraries/Source/Compression /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWDebug /Ireference/shims/sweep /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWLib /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WW3D2 /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWMath /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWSaveLoad /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/Wwutil /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWDownload /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWDebug /Ireference/open-bfme-1/Code/Libraries/Source/Compression /Ireference/shims/sweep
+// ?Clone@OBBoxRenderObjClass@@UBEPAVRenderObjClass@@XZ @ 0x001763D0 (78B).
+// Dedicated TU: boxrobj.cpp cannot take another row. Same recipe as the
+// landed AABox Clone twin (plain global ::new, not the W3DMPO pool glue).
+#include "rendobj.h"
+#include "boxrobj.h"
+
+// ?Clone@OBBoxRenderObjClass@@UBEPAVRenderObjClass@@XZ
+RenderObjClass * OBBoxRenderObjClass::Clone(void) const
+{
+	return ::new OBBoxRenderObjClass(*this);
+}
