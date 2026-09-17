c67d9662e8585246b79b3f22fae53410329e5a9c
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-17 04:56:53 +0000

    Recover opaque MI derived dtor Rva004BAB36 (0x4BAB36, 25B)

--- Code/GameEngine/Source/Common/Rva004B96CCDerived.cpp
+++ Code/GameEngine/Source/Common/Rva004B96CCDerived.cpp
@@ -40,3 +40,19 @@ public:
 Rva004B96E5::~Rva004B96E5()
 {
 }
+
+class Rva004BAB36_B2
+{
+public:
+	virtual void f2();
+};
+
+class Rva004BAB36 : public Rva004B96CC, public MiBase1, public Rva004BAB36_B2
+{
+public:
+	virtual ~Rva004BAB36();
+};
+
+Rva004BAB36::~Rva004BAB36()
+{
+}
