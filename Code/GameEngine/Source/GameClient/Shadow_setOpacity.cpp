2149628880527135567214c726d9ad7f4c2876b7
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-17 21:55:13 +0000

    Recover Shadow::setOpacity (0x3308F6, 159B) via BFME1 verbatim

---
+++ Code/GameEngine/Source/GameClient/Shadow_setOpacity.cpp
@@ -0,0 +1,43 @@
+// cl: /O1 /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc /Oy-
+//
+// ?setOpacity@Shadow@@QAEXH@Z, retail 0x003308F6, 159 bytes.
+// Dedicated TU.
+//
+// Battle for Middle-earth reference
+// (reference/open-bfme-1/Code/GameEngine/Source/GameClient/ShadowSetOpacity.cpp,
+// matched 169 bytes there).
+
+typedef int Int;
+typedef unsigned int UnsignedInt;
+typedef float Real;
+
+class Shadow
+{
+public:
+	void setOpacity(Int value);
+
+private:
+	char m_pad00[0x24];
+	UnsignedInt m_color;
+	Int m_diffuse;
+	UnsignedInt m_opacity;
+	char m_pad30[4];
+	UnsignedInt m_type;
+};
+
+void Shadow::setOpacity(Int value)
+{
+	m_opacity = value;
+
+	if (m_type & 0x1420)
+	{
+		m_diffuse = (m_color & 0x00ffffff) + (value << 24);
+	}
+	else if (m_type & 0x0840)
+	{
+		Real fvalue = (Real)m_opacity / 255.0f;
+		m_diffuse = (Int)((Real)(m_color & 0xff) * fvalue)
+			| ((Int)((Real)((m_color >> 8) & 0xff) * fvalue) << 8)
+			| ((Int)((Real)((m_color >> 16) & 0xff) * fvalue) << 16);
+	}
+}
