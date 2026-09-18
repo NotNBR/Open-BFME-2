5d76765cda8f09b3f66ad134c380d45b7e783267
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-18 19:15:51 +0000

    Recover bfmeGoVJC (0x0065E2E0, 154B) via BFME1 byte-identical donor

---
+++ Code/GameEngine/Source/Common/BfmeConv1382.cpp
@@ -0,0 +1,37 @@
+// Open-BFME5 conversions.
+
+class BfmeMsgVJC
+{
+public:
+	void bfmeRunVJC();
+	void bfmeSetVJC(const char *k, void *v);
+	void bfmeSet3VJC(const char *k, int v);
+	char m_bfmePad[0x1c];
+	int m_bfme1c;
+};
+
+extern void *g_bfmeVJC;
+
+class BfmeThingVJC
+{
+public:
+	void bfmeGoVJC(BfmeMsgVJC *m, int ratingMin, int ratingMax, int downloadMin, int downloadMax, void *a, void *b);
+	void bfmeSubVJC(BfmeMsgVJC *m, void *a, void *b);
+};
+
+void BfmeThingVJC::bfmeGoVJC(BfmeMsgVJC *m, int ratingMin, int ratingMax, int downloadMin, int downloadMax, void *a, void *b)
+{
+	void *g = g_bfmeVJC;
+	m->bfmeRunVJC();
+	m->m_bfme1c = 0x626c6f62;
+	m->bfmeSetVJC("TXN", g);
+	bfmeSubVJC(m, a, b);
+	if (ratingMin > -1)
+		m->bfmeSet3VJC("ratingMin", ratingMin);
+	if (ratingMax > -1)
+		m->bfmeSet3VJC("ratingMax", ratingMax);
+	if (downloadMin > -1)
+		m->bfmeSet3VJC("downloadMin", downloadMin);
+	if (downloadMax > -1)
+		m->bfmeSet3VJC("downloadMax", downloadMax);
+}
