90504681d73c2182f055ca018e119181ba4f7d64
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-18 20:52:11 +0000

    Recover addInt64 (0x00655F00, 93B) + Conv2048/Conv476/MeshNameOrTextSet via BFME1 donors

---
+++ Code/GameEngine/Source/Common/BfmeConv2048.cpp
@@ -0,0 +1,48 @@
+class BfmeVecFN
+{
+public:
+	unsigned char m_bfmeHeadFN[4];
+};
+
+class BfmeBufFN
+{
+public:
+	void bfmeAppendFN(const char *s);
+
+	unsigned char m_bfmeHeadFN[8];
+};
+
+class BfmeOwnerFN
+{
+public:
+	int bfmeFindFN(BfmeVecFN *v, const char *name);
+
+	unsigned char m_bfmeHeadFN[0x2a8];
+	BfmeVecFN m_bfmeVecFN;
+};
+
+class BfmeHostFN
+{
+public:
+	int bfmeAddFN(const char *name, const char *text);
+
+	void *m_bfmeVfFN;
+	BfmeOwnerFN *m_bfmeOwnerFN;
+	unsigned char m_bfmeGapFN[8];
+	BfmeBufFN *m_bfmeBufsFN;
+	int m_bfmeCountFN;
+};
+
+int BfmeHostFN::bfmeAddFN(const char *name, const char *text)
+{
+	int idx = m_bfmeOwnerFN->bfmeFindFN(&m_bfmeOwnerFN->m_bfmeVecFN, name);
+
+	if (idx == -1)
+		return -106;
+
+	BfmeBufFN *b = idx >= m_bfmeCountFN ? 0 : &m_bfmeBufsFN[idx];
+
+	b->bfmeAppendFN(text);
+
+	return 0;
+}
