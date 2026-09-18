713973247e9646a7cfa85432ebafb06a2a298bc7
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-18 22:11:19 +0000

    Recover bfmeProbeAAS (0x000397D0, 169B) via BFME1 near-miss (state +0x9F49 to +0x9F55 and GetWindowRect import)

---
+++ Code/GameEngine/Source/Common/BfmeConv2122.cpp
@@ -0,0 +1,57 @@
+// cl: /Oy-
+struct BfmeRectAAS
+{
+	int m_bfmeLeftAAS;
+	int m_bfmeTopAAS;
+	int m_bfmeRightAAS;
+	int m_bfmeBottomAAS;
+};
+
+extern "C"
+{
+	__declspec(dllimport) unsigned long __stdcall GetCurrentThreadId(void);
+	__declspec(dllimport) int __stdcall EnumThreadWindows(unsigned long id, int (__stdcall *fn)(void *, long), long param);
+	__declspec(dllimport) long __stdcall GetWindowLongA(void *hwnd, int index);
+	__declspec(dllimport) int __stdcall GetWindowRect(void *hwnd, BfmeRectAAS *rect);
+}
+
+int __stdcall EnumThreadWndProc(void *hwnd, long param);
+
+class BfmeHostAAS
+{
+public:
+	bool bfmeProbeAAS();
+
+	unsigned char m_bfmeHeadAAS[0x9f55];
+	char m_bfme9F55AAS;
+};
+
+bool BfmeHostAAS::bfmeProbeAAS()
+{
+	char state = m_bfme9F55AAS;
+
+	if (state != 0)
+		return state > 0;
+
+	void *found = 0;
+
+	EnumThreadWindows(GetCurrentThreadId(), EnumThreadWndProc, (long)&found);
+
+	if (found == 0)
+		return true;
+
+	m_bfme9F55AAS = (char)0xff;
+
+	if ((GetWindowLongA(found, -0x10) & 0xc00000) != 0)
+		m_bfme9F55AAS = 1;
+
+	BfmeRectAAS r;
+
+	if (GetWindowRect(found, &r) != 0)
+	{
+		if (r.m_bfmeRightAAS - r.m_bfmeLeftAAS < r.m_bfmeBottomAAS - r.m_bfmeTopAAS)
+			m_bfme9F55AAS = 1;
+	}
+
+	return m_bfme9F55AAS > 0;
+}
