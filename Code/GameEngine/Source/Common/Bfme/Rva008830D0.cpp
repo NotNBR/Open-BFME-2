f49592af03c0bd8a1c9427747302f822cd0d2882
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-18 23:22:55 +0000

    Recover d_008830d0 21B @0x0003CC80 via BFME1 donor; address-drift repair to string literal

---
+++ Code/GameEngine/Source/Common/Bfme/Rva008830D0.cpp
@@ -0,0 +1,35 @@
+// cl: /DNDEBUG /MD /EHs-c-
+
+class Rva008830D0Class
+{
+public:
+	virtual void v00();
+	virtual void v04();
+	virtual void v08();
+	virtual void v0C();
+	virtual void v10();
+	virtual void v14();
+	virtual void v18();
+	virtual void v1C();
+	virtual void v20();
+	virtual void v24();
+	virtual void v28();
+	virtual void v2C();
+	virtual void v30();
+	virtual void v34();
+	virtual void v38();
+	virtual void v3C();
+	virtual void v40();
+	virtual void v44();
+	virtual void v48();
+	virtual void v4C();
+	virtual void v50( void *a, int b );
+
+	Rva008830D0Class *d_008830d0( int dummy );
+};
+
+Rva008830D0Class *Rva008830D0Class::d_008830d0( int dummy )
+{
+	v50( (void *)"0x", 0x10 );
+	return this;
+}
