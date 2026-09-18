96b0411b44d6117065a9304dfbaffdc5da21c53b
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-18 13:15:56 +0000

    Recover hasSeenLogoMovies (0x002E498F, 92B) via BFME1 verbatim twin of DisallowNonAsianText

---
+++ Code/GameEngine/Source/GameClient/GUI/GUICallbacks/Menus/OptionPreferences_hasSeenLogoMovies.cpp
@@ -0,0 +1,83 @@
+// cl: /O1 /GX- /DNDEBUG /MD /D_STLP_USE_STATIC_LIB
+// stlport
+//
+// ?hasSeenLogoMovies@OptionPreferences@@QAE_NXZ, retail 0x002E498F,
+// 92 bytes. Dedicated TU.
+//
+// BFME1 OptionPreferences::hasSeenLogoMovies verbatim
+// (reference/open-bfme-1 Code/GameEngine/Source/Common/OptionPreferencesFlags.cpp:
+// key "HasSeenLogoMovies", miss-false, _strcmpi against "yes"). Same string
+// plumbing as CustomMatchPreferences_getDisallowNonAsianText.cpp (TU-local
+// StringBase/AsciiString, real STLport map with the 0x1F8437 find worker
+// pinned, StringBase ctor and releaseBuffer pinned, _strcmpi via the 0xBBA518
+// import). The AL-width neg/sbb/inc tail needs `register Bool match`, the
+// same load-bearing spelling as the Disallow twins.
+
+#include <map>
+#include <stdlib.h>
+#include <string.h>
+
+typedef bool Bool;
+typedef int Int;
+
+template <typename T> struct BfmeStringData
+{
+	int refCount;
+	unsigned short length;
+	unsigned short capacity;
+	T text[1];
+};
+
+template <typename T> class StringBase
+{
+	friend class AsciiString;
+private:
+	StringBase(const T *text);
+	StringBase(const StringBase<T> &other);
+	~StringBase() { releaseBuffer(); }
+	void releaseBuffer();
+	BfmeStringData<T> *m_data;
+
+public:
+	const T *str() const { return m_data ? &m_data->text[0] : (const T *)""; }
+};
+
+class AsciiString : private StringBase<char>
+{
+public:
+	AsciiString(const char *text) : StringBase<char>(text) {}
+	AsciiString(const AsciiString &other);
+	~AsciiString() {}
+};
+
+bool operator<(const AsciiString &left, const AsciiString &right);
+
+namespace _STL
+{
+template <> struct less<AsciiString>
+{
+	bool operator()(const AsciiString &left, const AsciiString &right) const
+	{
+		return left < right;
+	}
+};
+}
+
+typedef _STL::map<AsciiString, AsciiString> AsciiPreferenceMap;
+
+class OptionPreferences : public AsciiPreferenceMap
+{
+public:
+	virtual ~OptionPreferences();
+	Bool hasSeenLogoMovies();
+};
+
+Bool OptionPreferences::hasSeenLogoMovies(void)
+{
+	OptionPreferences::const_iterator it = find("HasSeenLogoMovies");
+	if (it == end())
+		return false;
+
+	register Bool match = (_strcmpi(it->second.str(), "yes") == 0);
+	return match;
+}
