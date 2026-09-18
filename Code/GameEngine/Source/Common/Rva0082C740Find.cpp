55f40495e7fc75a50ec2c706b65e86a3bbf90599
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-18 23:23:55 +0000

    Recover allocate 75B @0x0066DEB0 via BFME1 donor verbatim; single ICF pick

---
+++ Code/GameEngine/Source/Common/Rva0082C740Find.cpp
@@ -0,0 +1,8 @@
+// cl: /Od /Ob0 /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc
+
+int *rva0082BBC0Find(int *node, int value);
+
+int *rva0082C740Find(int *node, int value)
+{
+	return rva0082BBC0Find(node, value);
+}
