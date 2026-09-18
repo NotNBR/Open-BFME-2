229ceceb7da91012e18e74e81223084e712f040b
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-18 22:55:42 +0000

    Recover rva002E27F0 30B @0x000383D0 via BFME1 donor (ICF pick by 3-arg arity; vtable slot 0x80 not 0x94)

---
+++ Code/GameEngine/Source/Common/Rva002E27F0Forward.cpp
@@ -0,0 +1,51 @@
+// cl: /DNDEBUG /MD /EHs-c-
+
+class BFMEIndexBufferDebugClass
+{
+public:
+	virtual void slot00();
+	virtual void slot04();
+	virtual void slot08();
+	virtual void slot0C();
+	virtual void slot10();
+	virtual void slot14();
+	virtual void slot18();
+	virtual void slot1C();
+	virtual void slot20();
+	virtual void slot24();
+	virtual void slot28();
+	virtual void slot2C();
+	virtual void slot30();
+	virtual void slot34();
+	virtual void slot38();
+	virtual void slot3C();
+	virtual void slot40();
+	virtual void slot44();
+	virtual void slot48();
+	virtual void slot4C();
+	virtual void slot50();
+	virtual void slot54();
+	virtual void slot58();
+	virtual void slot5C();
+	virtual void slot60();
+	virtual void slot64();
+	virtual void slot68();
+	virtual void slot6C();
+	virtual void slot70();
+	virtual void slot74();
+	virtual void slot78();
+	virtual void slot7C();
+	virtual void slot80(void *a, void *b, void *c);
+	virtual void slot84();
+	virtual void slot88();
+	virtual void slot8C();
+	virtual void slot90();
+	virtual void slot94();
+};
+
+extern BFMEIndexBufferDebugClass *g_BFMEIndexBufferDebug;
+
+void rva002E27F0(void *a, void *b, void *c)
+{
+	g_BFMEIndexBufferDebug->slot80(a, b, c);
+}
