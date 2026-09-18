290881dbd644dc09113dcd5bd59fdb83ae99612d
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-18 20:53:18 +0000

    Recover bfmeAddFO (0x0066DCA0, 82B) via BFME1 byte-identical donor

---
+++ Code/GameEngine/Source/Common/BfmeGrokOdIf0.cpp
@@ -0,0 +1,35 @@
+// cl: /Od
+
+struct Gen_0082ad50
+{
+	void m();
+};
+
+Gen_0082ad50 g_bfme0130b250;
+Gen_0082ad50 g_bfme0130b254;
+
+class BfmeIf0VMJ
+{
+public:
+	void grokIf0();
+};
+
+class BfmeIf1VMK
+{
+public:
+	void grokIf1();
+};
+
+void BfmeIf0VMJ::grokIf0()
+{
+	int n1;
+	if (0)
+		g_bfme0130b250.m();
+}
+
+void BfmeIf1VMK::grokIf1()
+{
+	int n1;
+	if (1)
+		g_bfme0130b254.m();
+}
