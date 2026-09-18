db1c6788cf6401eaa27fb99635162ae43caecfa0
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-18 23:03:43 +0000

    Recover Rva0033A4F0 copy 27B @0x00204B33 via BFME1 donor verbatim; single ICF pick

---
+++ Code/GameEngine/Source/Common/Rva0033A4F0Copy.cpp
@@ -0,0 +1,24 @@
+// cl: /O2 /Ob0
+
+class Rva0033CopyBase
+{
+public:
+	Rva0033CopyBase(const Rva0033CopyBase &other);
+
+private:
+	char m_pad[8];
+};
+
+class Rva0033A4F0 : public Rva0033CopyBase
+{
+	char m_08;
+
+public:
+	Rva0033A4F0(const Rva0033A4F0 &other);
+};
+
+Rva0033A4F0::Rva0033A4F0(const Rva0033A4F0 &other)
+	: Rva0033CopyBase(other)
+	, m_08(other.m_08)
+{
+}
