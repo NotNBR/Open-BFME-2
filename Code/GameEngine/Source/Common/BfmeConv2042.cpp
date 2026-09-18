0701b95631494d8cb450962a5e6f0f072c029805
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-18 15:55:55 +0000

    Recover bfmeStepsEU ellipse-step counter (0x0073B750, 140B) via BFME1 donor with ceil import

---
+++ Code/GameEngine/Source/Common/BfmeConv2042.cpp
@@ -0,0 +1,64 @@
+extern "C" double sqrt(double x);
+
+#pragma intrinsic(sqrt)
+
+extern "C" __declspec(dllimport) double ceil(double x);
+
+class BfmeShapeEU
+{
+public:
+	int m_bfmeKindEU;
+	unsigned char m_bfmePadEU[4];
+	float m_bfmeAEU;
+	float m_bfmeBEU;
+};
+
+class BfmeHostEU
+{
+public:
+	int bfmeStepsEU(const BfmeShapeEU *s);
+
+	unsigned char m_bfmeHeadEU[0x20];
+	float m_bfmeScaleEU;
+};
+
+int BfmeHostEU::bfmeStepsEU(const BfmeShapeEU *s)
+{
+	float t;
+	int n;
+
+	switch (s->m_bfmeKindEU)
+	{
+		case 0:
+		case 1:
+		{
+			t = (float)ceil((s->m_bfmeAEU + s->m_bfmeAEU) * m_bfmeScaleEU);
+
+			__asm fld t
+			__asm fistp n
+
+			break;
+		}
+
+		case 2:
+		{
+			float d = (float)sqrt(s->m_bfmeAEU * s->m_bfmeAEU + s->m_bfmeBEU * s->m_bfmeBEU);
+			t = (float)ceil((d + d) * m_bfmeScaleEU);
+
+			__asm fld t
+			__asm fistp n
+
+			break;
+		}
+
+		default:
+			return 4;
+	}
+
+	int c = n + 1;
+
+	if (c > 100)
+		c = 100;
+
+	return c * c;
+}
