9ae531b1ab96f7621fd45ce84e8f369ebb7b70f6
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-18 03:11:25 +0000

    Recover getRefreshNet (0x2E4DAE, 100B) via TurnOffMessenger twin

---
+++ Code/GameEngine/Source/GameClient/GUI/GUICallbacks/Menus/OptionPreferences_getRefreshNet.cpp
@@ -0,0 +1,91 @@
+// cl: /O1 /GX- /DNDEBUG /MD /D_STLP_USE_STATIC_LIB
+// stlport
+//
+// ?getRefreshNet@OptionPreferences@@QAE_NXZ, retail 0x002E4DAE,
+// 100 bytes. Dedicated TU.
+//
+// ZH OptionPreferences getter shape (GeneralsMD/.../GUICallbacks/Menus/
+// OptionsMenu.cpp) via the getTurnOffMessengerInGame twin (same plumbing;
+// the miss default is TheGlobalData's byte retail-measured at +0xB6C;
+// the yes/no test calls strcmp through the CRT thunk at 0x006291C6).
+
+#include <map>
+#include <stdlib.h>
+#include <string.h>
+
+typedef bool Bool;
+typedef int Int;
+
+template <typename T> struct BfmeStringData
+{
+	int refCount;
+	unsigned short length;
+	unsigned short capacity;
+	T text[1];
+};
+
+template <typename T> class StringBase
+{
+	friend class AsciiString;
+private:
+	StringBase(const T *text);
+	StringBase(const StringBase<T> &other);
+	~StringBase() { releaseBuffer(); }
+	void releaseBuffer();
+	BfmeStringData<T> *m_data;
+
+public:
+	const T *str() const { return m_data ? &m_data->text[0] : (const T *)""; }
+};
+
+class AsciiString : private StringBase<char>
+{
+public:
+	AsciiString(const char *text) : StringBase<char>(text) {}
+	AsciiString(const AsciiString &other) : StringBase<char>(other) {}
+	~AsciiString() {}
+};
+
+bool operator<(const AsciiString &left, const AsciiString &right);
+
+namespace _STL
+{
+template <> struct less<AsciiString>
+{
+	bool operator()(const AsciiString &left, const AsciiString &right) const
+	{
+		return left < right;
+	}
+};
+}
+
+typedef _STL::map<AsciiString, AsciiString> AsciiPreferenceMap;
+
+class GlobalData
+{
+public:
+	char m_pad[0xB6C];
+	Bool m_refreshNet;
+};
+
+extern GlobalData *TheGlobalData;
+
+class OptionPreferences : public AsciiPreferenceMap
+{
+public:
+	virtual ~OptionPreferences();
+	Bool getRefreshNet();
+};
+
+// ?getRefreshNet@OptionPreferences@@QAE_NXZ
+Bool OptionPreferences::getRefreshNet(void)
+{
+	OptionPreferences::const_iterator it = find("RefreshNet");
+	if (it == end())
+		return TheGlobalData->m_refreshNet;
+
+	if (strcmp(it->second.str(), "yes") == 0) {
+		return true;
+	}
+	return false;
+}
