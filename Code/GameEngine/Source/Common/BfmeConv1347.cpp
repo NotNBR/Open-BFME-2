90bec801444d5bf3ad71dde1a35c6a4a80ebf7da
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-18 23:50:09 +0000

    Recover bfmeGoAUZA 43B @0x00149540 via BFME1 donor with layout repair; single ICF pick

---
+++ Code/GameEngine/Source/Common/BfmeConv1347.cpp
@@ -0,0 +1,40 @@
+// Open-BFME5 conversions.
+
+struct BfmeHandleUZA
+{
+	int m_bfmeValue;
+};
+
+class BfmeSubAUZA
+{
+public:
+	BfmeHandleUZA bfmeGetAUZA(int a, int b) const;
+};
+
+class BfmeSubBUZA
+{
+public:
+	BfmeHandleUZA bfmeGetBUZA(int a, int b) const;
+};
+
+class BfmeThingAUZA
+{
+public:
+	BfmeHandleUZA bfmeGoAUZA(int a, int b) const;
+	char m_bfmePad[0x94];
+	BfmeSubAUZA *m_bfmeSub;
+};
+
+BfmeHandleUZA BfmeThingAUZA::bfmeGoAUZA(int a, int b) const
+{
+	volatile int m_bfmeDead = 0;
+	return m_bfmeSub->bfmeGetAUZA(a, b);
+}
+
+class BfmeThingBUZA
+{
+public:
+	BfmeHandleUZA bfmeGoBUZA(int a, int b) const;
+	char m_bfmePad[0x9c];
+	BfmeSubBUZA *m_bfmeSub;
+};
