eabaf9a30962cdbe1bcdf5b0fb56a09d2717a102
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-18 23:11:35 +0000

    Recover Rva007E3430 ctor 20B @0x00106A19 via BFME1 donor verbatim

--- Code/GameEngine/Source/Common/R2ZeroingConstructors.cpp
+++ Code/GameEngine/Source/Common/R2ZeroingConstructors.cpp
@@ -44,3 +44,20 @@ Rva003B7160::Rva003B7160()
 	m_at08 = 0;
 	m_at0C = 0;
 }
+
+class Rva007E3430
+{
+public:
+	int m_at00;
+	int m_at04;
+	int m_at08;
+	int m_at0C;
+	Rva007E3430();
+};
+Rva007E3430::Rva007E3430()
+{
+	m_at00 = 0;
+	m_at04 = 0;
+	m_at08 = 4;
+	m_at0C = 0;
+}
