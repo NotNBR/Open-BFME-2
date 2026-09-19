9aa0e2b4aebe9fff1e544b63665abb0d56e9c3cb
Peppy Penguin <peppy@example.com>
2026-09-19 20:22:17 +0000

    Recover Log errorFrom 160B @0x00658930 via BFME1 donor

--- Code/GameEngine/Source/GameNetwork/Rva007EB920Assert.cpp
+++ Code/GameEngine/Source/GameNetwork/Rva007EB920Assert.cpp
@@ -26,6 +26,7 @@ public:
 	virtual void v0();
 	virtual void emit(char *buf);
 	int assertFailed(const char *expr, const char *file, int line);
+	int errorFrom(int code, const char *from, const char *file, int line);
 };
 
 int Rva007EB8B0Log::assertFailed(const char *expr, const char *file, int line)
@@ -38,6 +39,22 @@ int Rva007EB8B0Log::assertFailed(const char *expr, const char *file, int line)
 			"\\views\\feslbuild_main\\jabba\\fesl\\source\\debug.cpp",
 			137);
 	emit(buf);
+	return 0;
+}
+
+int Rva007EB8B0Log::errorFrom(int code, const char *from, const char *file, int line)
+{
+	char buf[0x180];
+	if (code)
+	{
+		sprintf(buf, "error %d from '%.128s' at %.128s:%ld\n", code, from, file, line);
+		if (!(strlen(buf) < 0x180))
+			g_Va0130A5A0->fail(
+				"strlen(buf) < sizeof(buf)",
+				"\\views\\feslbuild_main\\jabba\\fesl\\source\\debug.cpp",
+				161);
+		emit(buf);
+	}
 	return 0;
 }
 
