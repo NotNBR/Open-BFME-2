5c44ddf277a35b1011492e46ff0a3cd41fd26a44
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-19 15:17:56 +0000

    Recover TerrainLogic parseWaypointData (0x0027FFF3, 60B) via ZH donor; pins readInt and addWaypointLink

---
+++ Code/GameEngine/Source/GameLogic/Map/TerrainLogic_parseWaypointData.cpp
@@ -0,0 +1,40 @@
+// cl: /O1 /DNDEBUG /MD /EHsc
+// ?parseWaypointData@TerrainLogic@@IAE_NAAVDataChunkInput@@PAUDataChunkInfo@@PAX@Z,
+// retail 0x0027FFF3, 60 bytes. Dedicated TU.
+//
+// Battle for Middle-earth 2 keeps the Zero Hour skeleton verbatim:
+// three readInt calls plus addWaypointLink per link; the end-of-chunk
+// assert vanishes in NDEBUG so retail is a frameless loop plus true.
+
+typedef int Int;
+
+struct DataChunkInfo;
+
+class DataChunkInput
+{
+public:
+	Int readInt();
+};
+
+class __declspec(novtable) TerrainLogic
+{
+public:
+	virtual void slot00() = 0;
+
+protected:
+	bool parseWaypointData(DataChunkInput &file, DataChunkInfo *info, void *userData);
+	void addWaypointLink(Int id1, Int id2);
+};
+
+// ?parseWaypointData@TerrainLogic@@IAE_NAAVDataChunkInput@@PAUDataChunkInfo@@PAX@Z
+bool TerrainLogic::parseWaypointData(DataChunkInput &file, DataChunkInfo *info, void *userData)
+{
+	Int numWaypointLinks = file.readInt();
+	Int i;
+	for (i = 0; i < numWaypointLinks; i++) {
+		Int waypoint1 = file.readInt();
+		Int waypoint2 = file.readInt();
+		addWaypointLink(waypoint1, waypoint2);
+	}
+	return true;
+}
