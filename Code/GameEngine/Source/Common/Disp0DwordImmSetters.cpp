4da6542e866185399a696ff9471146744d27344b
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-19 09:20:42 +0000

    Recover Disp0 dword imm-setter 7B @0x0007DEA8; triple last

--- Code/GameEngine/Source/Common/Disp0DwordImmSetters.cpp
+++ Code/GameEngine/Source/Common/Disp0DwordImmSetters.cpp
@@ -62,3 +62,16 @@ void Rva0007DEA1DwordImmSetter::apply()
 {
 	m_value = 0x00BC6EEC;
 }
+
+class Rva0007DEA8DwordImmSetter
+{
+public:
+	void apply();
+
+	unsigned int m_value;
+};
+
+void Rva0007DEA8DwordImmSetter::apply()
+{
+	m_value = 0x00BC6F04;
+}
