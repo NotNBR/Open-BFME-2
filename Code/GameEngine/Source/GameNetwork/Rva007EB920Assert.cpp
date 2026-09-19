1d35f7d737eaf883572eb311de81665d7242a760
vvvalenntinnn-collab <320846852+vvvalenntinnn-collab@users.noreply.github.com>
2026-09-18 19:39:52 -0700

    Recover FESL assert formatter body

---
+++ Code/GameEngine/Source/GameNetwork/Rva007EB920Assert.cpp
@@ -0,0 +1,43 @@
+// cl: /GS
+// 0x007EB920 / 0x007EB9C0: FESL debug.cpp assert/error formatters. sprintf
+// into a 0x180-byte stack buffer, fail if strlen >= sizeof(buf), emit vslot 1.
+
+struct Rva007EB810Diag
+{
+	virtual void v0();
+	virtual void v1();
+	virtual void v2();
+	virtual void fail(const char *expr, const char *file, int line);
+};
+
+extern Rva007EB810Diag *g_Va0130A5A0;
+
+extern "C"
+{
+	int __cdecl sprintf(char *buf, const char *fmt, ...);
+	unsigned int __cdecl strlen(const char *string);
+}
+
+#pragma intrinsic(strlen)
+
+class Rva007EB8B0Log
+{
+public:
+	virtual void v0();
+	virtual void emit(char *buf);
+	int assertFailed(const char *expr, const char *file, int line);
+};
+
+int Rva007EB8B0Log::assertFailed(const char *expr, const char *file, int line)
+{
+	char buf[0x180];
+	sprintf(buf, "assert '%.128s' at %.128s:%ld failed\n", expr, file, line);
+	if (!(strlen(buf) < 0x180))
+		g_Va0130A5A0->fail(
+			"strlen(buf) < sizeof(buf)",
+			"\\views\\feslbuild_main\\jabba\\fesl\\source\\debug.cpp",
+			137);
+	emit(buf);
+	return 0;
+}
+
