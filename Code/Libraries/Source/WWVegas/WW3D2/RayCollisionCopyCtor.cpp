afbd48644d61af62eb8d6688007fb297b537df38
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-19 17:14:47 +0000

    Recover RayCollisionTestClass transformed-copy ctor 64B @0x001493B0 via ZH coltest.h inline + flag42; dedicated /O2 TU split from /O1 primary

---
+++ Code/Libraries/Source/WWVegas/WW3D2/RayCollisionCopyCtor.cpp
@@ -0,0 +1,30 @@
+// cl: /Ireference/shims/bfme2ray /Ireference/shims/bfme2scene /Ireference/shims/bfme2renderobj /arch:SSE /G7 /DNDEBUG /MD /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWMath /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWLib /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWSaveLoad /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WW3D2 /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/Wwutil /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWDownload /Ireference/open-bfme-1/Code/Libraries/Source/Compression /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWDebug /Ireference/shims/sweep /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWLib /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WW3D2 /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWMath /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWSaveLoad /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/Wwutil /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWDownload /Ireference/open-bfme-1/Code/Libraries/Source/WWVegas/WWDebug /Ireference/open-bfme-1/Code/Libraries/Source/Compression /Ireference/shims/sweep
+/*
+**	Command & Conquer Generals Zero Hour(tm)
+**	Copyright 2025 Electronic Arts Inc.
+**
+**	This program is free software: you can redistribute it and/or modify
+**	it under the terms of the GNU General Public License as published by
+**	the Free Software Foundation, either version 3 of the License, or
+**	(at your option) any later version.
+**
+**	This program is distributed in the hope that it will be useful,
+**	but WITHOUT ANY WARRANTY; without even the implied warranty of
+**	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**	GNU General Public License for more details.
+**
+**	You should have received a copy of the GNU General Public License
+**	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include "rendobj.h"
+#include "aabox.h"
+#include "coltest.h"
+
+// Transformed-copy constructor. Split from RayCollisionConstructor.cpp:
+// the primary matches at /O1, this body needs default (/O2) scheduling
+// (base values kept in ecx/edx, +8 store sunk past the Set arg setup).
+// ZH coltest.h inline port plus the BFME2 extra flag.
+RayCollisionTestClass::RayCollisionTestClass(const RayCollisionTestClass & raytest, const Matrix3D & tm) :
+    CollisionTestClass(raytest), Ray(raytest.Ray, tm),
+    CheckTranslucent(raytest.CheckTranslucent), CheckHidden(raytest.CheckHidden), _bfme_flag42(false)
+{}
