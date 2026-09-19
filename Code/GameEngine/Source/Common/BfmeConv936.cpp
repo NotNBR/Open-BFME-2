a2e94c2dd588e678d3ae0be9d43f7150bb715cc8
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-19 05:15:49 +0000

    Recover bfmeGo936B 19B @0x00143C00 via BFME1 donor; trimmed TU

---
+++ Code/GameEngine/Source/Common/BfmeConv936.cpp
@@ -0,0 +1,31 @@
+// Open-BFME5 conversions (trimmed to the placed 936B body; the other three
+// are declared-only here).
+
+void __stdcall bfmeElem936B(void *p);
+void __stdcall bfmeVecDtor936B(void *p, unsigned int size, int count, void (__stdcall *dtor)(void *));
+
+class BfmeThing936B
+{
+public:
+	void bfmeGo936B(void);
+};
+
+// ?bfmeGo936B@BfmeThing936B@@QAEXXZ
+void BfmeThing936B::bfmeGo936B(void)
+{
+	bfmeVecDtor936B(this, 4, 0x80, bfmeElem936B);
+}
+
+class BfmeThing936F
+{
+public:
+	void bfmeGo936F(void);
+};
+
+void bfmeGo936C(void);
+
+class BfmeThing936G
+{
+public:
+	BfmeThing936G *bfmeGo936G(void);
+};
