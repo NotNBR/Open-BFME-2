b26308da3d5a73fd0fcfef852d0bdc0e130f711a
vvvalenntinnn-collab <320846852+vvvalenntinnn-collab@users.noreply.github.com>
2026-09-18 19:47:29 -0700

    Recover common 82BC30 lane bodies

---
+++ Code/GameEngine/Source/Common/Rva0082BC30Empty.cpp
@@ -0,0 +1,48 @@
+// cl: /Od
+void rva0082BC40Swap(int *a, int *b)
+{
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+class Rva0082BFD0Slot
+{
+public:
+	Rva0082BFD0Slot *assignFrom(const int *src);
+
+private:
+	int m_value;
+};
+
+Rva0082BFD0Slot *Rva0082BFD0Slot::assignFrom(const int *src)
+{
+	int tmp = *src;
+	m_value = tmp;
+	return this;
+}
+
+int g_rva0082B120;
+
+int rva0082B120Set(int value)
+{
+	int old = g_rva0082B120;
+	g_rva0082B120 = value;
+	return old;
+}
+
+int *rva0082BBC0Find(int *node, int value)
+{
+	while (node != 0 && *node != value)
+		node = (int *)*node;
+	return node;
+}
+
+int rva0082BAF0Count(int *node)
+{
+	int count = 0;
+	for (; node != 0; node = (int *)*node)
+		count++;
+	return count;
+}
+
