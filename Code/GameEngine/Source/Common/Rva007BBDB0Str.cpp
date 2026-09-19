c0259edae0526f19993f5b096074020b1b4a6aed
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-19 04:31:37 +0000

    Recover 2 str getters (34B) via BFME1 donor; trimmed TU

---
+++ Code/GameEngine/Source/Common/Rva007BBDB0Str.cpp
@@ -0,0 +1,42 @@
+// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc
+
+class Rva007BBDB0Str
+{
+public:
+	const char *str();
+
+	char m_pad[8];
+	char *m_data;
+};
+
+class Rva007BBDD0Str
+{
+public:
+	const char *str();
+
+	char m_pad[0x10];
+	char *m_data;
+};
+
+const char *Rva007BBDD0Str::str()
+{
+	if (m_data)
+		return m_data + 8;
+	return "";
+}
+
+class Rva003879F0Str
+{
+public:
+	const char *str();
+
+	char m_pad[4];
+	char *m_data;
+};
+
+const char *Rva003879F0Str::str()
+{
+	if (m_data)
+		return m_data + 8;
+	return "";
+}
