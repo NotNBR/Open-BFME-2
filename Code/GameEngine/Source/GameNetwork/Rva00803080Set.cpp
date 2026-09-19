e5e801e2c8573e18fff4c1ae38efcb8ddbf0c9e5
Peppy Penguin <peppy@penguin.test>
2026-09-19 21:40:36 +0000

    Recover Rva00803080::set 29B @0x0066F0E0 via BFME1 donor resolved by address map
    Donor reference/open-bfme-1/.../Rva00803080Set.cpp is byte-identical
    after a one-constant repair (vtable 0x0112C6F4 -> 0x00CE3ED4 from target
    bytes). Zero pins, zero calls.

---
+++ Code/GameEngine/Source/GameNetwork/Rva00803080Set.cpp
@@ -0,0 +1,23 @@
+// cl: /O2 /Ob0
+
+class Rva00803080
+{
+	void *m_00;
+	int m_04;
+	int m_08;
+	int m_0C;
+	int m_10;
+
+public:
+	Rva00803080 &set(int a);
+};
+
+Rva00803080 &Rva00803080::set(int a)
+{
+	m_04 = a;
+	m_00 = (void *)0x00CE3ED4;
+	m_08 = 0;
+	m_0C = 0;
+	m_10 = 0;
+	return *this;
+}
