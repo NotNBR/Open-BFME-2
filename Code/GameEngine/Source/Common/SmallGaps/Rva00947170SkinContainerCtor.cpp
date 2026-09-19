d68f30e55331ef9a8620a34c2fb8ed2f0a3b191b
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-19 01:02:01 +0000

    Recover Rva00947170SkinContainer ctor 84B @0x001455B0 via BFME1 donor with base-size repair 0xF0 to 0xE8

---
+++ Code/GameEngine/Source/Common/SmallGaps/Rva00947170SkinContainerCtor.cpp
@@ -0,0 +1,22 @@
+// ??0Rva00947170SkinContainer@@QAE@_N0@Z
+// cl: /DNDEBUG /MD /EHsc
+class DX8FVFCategoryContainer {
+public:
+	DX8FVFCategoryContainer(unsigned int fvf, bool sorting);
+	virtual ~DX8FVFCategoryContainer();
+	char m_body[0xe8 - 4];
+};
+class Rva00947170SkinContainer : public DX8FVFCategoryContainer {
+public:
+	int m_f0;
+	int m_f4;
+	int m_f8;
+	int m_fc;
+	bool m_skinned;
+	Rva00947170SkinContainer(bool sorting, bool skinned);
+};
+Rva00947170SkinContainer::Rva00947170SkinContainer(bool sorting, bool skinned)
+	: DX8FVFCategoryContainer(skinned ? 0x252 : 0x112, sorting),
+	m_f0(0), m_f4(0), m_f8(0), m_fc(0), m_skinned(skinned)
+{
+}
