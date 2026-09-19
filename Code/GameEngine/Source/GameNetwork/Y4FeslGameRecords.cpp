261124a1a8f5d545382afd0bf0198d66f8bb348f
Peppy Penguin <peppy@example.com>
2026-09-19 21:26:48 +0000

    Recover Rva007FBFB0 game-record lookup 106B @0x006684B0 via BFME1 donor

--- Code/GameEngine/Source/GameNetwork/Y4FeslGameRecords.cpp
+++ Code/GameEngine/Source/GameNetwork/Y4FeslGameRecords.cpp
@@ -58,6 +58,7 @@ public:
 	Rva007FBEF0GameRecord( Rva007E8810Message *msg );
 	bool Rva007FBE80( const char *key, char *dest, unsigned int destSize );
 	bool Rva007FBF40( const char *key, char *dest, unsigned int destSize );
+	bool Rva007FBFB0( const char *key, char *dest, unsigned int destSize );
 
 	int m_lid;
 	int m_gid;
@@ -93,6 +94,17 @@ bool Rva007FBEF0GameRecord::Rva007FBF40( const char *key, char *dest,
 	if( key == 0 || destSize <= 0 )
 		return false;
 	sprintf( name, "D-%s", key );
+	return m_msg->getString( name, dest, destSize );
+}
+
+bool Rva007FBEF0GameRecord::Rva007FBFB0( const char *key, char *dest,
+	unsigned int destSize )
+{
+	char name[ 0x40 ];
+
+	if( key == 0 || destSize <= 0 )
+		return false;
+	sprintf( name, "P-%s", key );
 	return m_msg->getString( name, dest, destSize );
 }
 
