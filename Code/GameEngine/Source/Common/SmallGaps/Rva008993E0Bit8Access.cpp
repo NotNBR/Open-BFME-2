fd7da69ae82327bdbf3bd705015296474237ee77
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-19 06:47:15 +0000

    Recover setBit8 32B @0x006D6440 via BFME1 donor; trimmed TU

---
+++ Code/GameEngine/Source/Common/SmallGaps/Rva008993E0Bit8Access.cpp
@@ -0,0 +1,11 @@
+// ?setBit8@Rva008993E0Owner@@QAEXH@Z
+struct Rva008993E0Owner {
+	char m_pad[0x1c];
+	union { unsigned int m_flags; struct { unsigned int m_low : 8; unsigned int m_bit8 : 1; }; };
+	void setBit8(int value);
+	int getBit8();
+};
+void Rva008993E0Owner::setBit8(int value)
+{
+	m_bit8 = value != 0;
+}
