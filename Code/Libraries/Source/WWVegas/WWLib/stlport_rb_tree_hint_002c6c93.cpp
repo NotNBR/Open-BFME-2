55e7b123488af7155e27548bab12ab0fa8f0a5ff
NotNBR <331651539+NotNBR@users.noreply.github.com>
2026-09-20 13:06:43 +0200

    Recover tree 002c6c93 node creation
    Specialize the STLport node creator locally to use the established static BFME byte allocator and its placement-construction helper. Retail has no cleanup catch path; retain the existing comparator and insertion layouts.
    
    Verify 34 bytes at 0x001F8485 with exact resolved equality; no unresolved relocations or masked aliases.

--- Code/Libraries/Source/WWVegas/WWLib/stlport_rb_tree_hint_002c6c93.cpp
+++ Code/Libraries/Source/WWVegas/WWLib/stlport_rb_tree_hint_002c6c93.cpp
@@ -17,5 +17,23 @@ bool operator<(const AsciiString &, const AsciiString &);
 struct TreeHintPayload001F8ACB { char m_body[4]; };
 typedef _STL::pair<const AsciiString, TreeHintPayload001F8ACB> TreeHintPair001F8ACB;
 typedef _STL::_Rb_tree<AsciiString, TreeHintPair001F8ACB, _STL::_Select1st<TreeHintPair001F8ACB>, _STL::less<AsciiString>, _STL::allocator<TreeHintPair001F8ACB> > TreeHint001F8ACB;
-template TreeHint001F8ACB::iterator TreeHint001F8ACB::insert_unique(TreeHint001F8ACB::iterator, const TreeHintPair001F8ACB &);
+// BFME replaces STLport allocation with a static byte allocator (RVA 0x307F0).
+namespace _STL {
+template <> class allocator<char> {
+public:
+    static char *allocate(unsigned int bytes, const void *hint);
+};
+}
 
+// Retail 0x001F8485 allocates the node, then constructs its value.
+// Unlike stock STLport, this retail body has no allocation-cleanup catch path.
+// ?_M_create_node@?$_Rb_tree@VAsciiString@@U?$pair@$$CBVAsciiString@@UTreeHintPayload001F8ACB@@@_STL@@U?$_Select1st@U?$pair@$$CBVAsciiString@@UTreeHintPayload001F8ACB@@@_STL@@@3@U?$less@VAsciiString@@@3@V?$allocator@U?$pair@$$CBVAsciiString@@UTreeHintPayload001F8ACB@@@_STL@@@3@@_STL@@IAEPAU?$_Rb_tree_node@U?$pair@$$CBVAsciiString@@UTreeHintPayload001F8ACB@@@_STL@@@2@ABU?$pair@$$CBVAsciiString@@UTreeHintPayload001F8ACB@@@2@@Z
+template <>
+TreeHint001F8ACB::_Link_type TreeHint001F8ACB::_M_create_node(const TreeHintPair001F8ACB &value)
+{
+    _Link_type node = (_Link_type)_STL::allocator<char>::allocate(sizeof(_STL::_Rb_tree_node<TreeHintPair001F8ACB>), 0);
+    _STL::_Construct(&node->_M_value_field, value);
+    return node;
+}
+
+template TreeHint001F8ACB::iterator TreeHint001F8ACB::insert_unique(TreeHint001F8ACB::iterator, const TreeHintPair001F8ACB &);
