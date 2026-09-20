57180dcf496891462ffb70b3338542b4c9fe07f9
NotNBR <331651539+NotNBR@users.noreply.github.com>
2026-09-20 13:32:57 +0200

    Recover refcounted tree 00221e3a node creation
    Use the BFME1/STLport insertion family with the independently verified BFME2 AsciiString comparator, 24-byte node layout, and pair-copy chain. The mapped pointer copy increments its non-null pointee at offset 4; leave the unknown application type and destructor behavior opaque.
    
    Full 34-byte body at 0x00221BF3 matches after relocation resolution. No unresolved calls or masked aliases.

--- Code/Libraries/Source/WWVegas/WWLib/stlport_rb_tree_hint_00221e3a.cpp
+++ Code/Libraries/Source/WWVegas/WWLib/stlport_rb_tree_hint_00221e3a.cpp
@@ -23,4 +23,19 @@ struct TreeHintRef00221D6B {
 
 typedef _STL::pair<const AsciiString, TreeHintRef00221D6B> TreeHintPair00221D6B;
 typedef _STL::_Rb_tree<AsciiString, TreeHintPair00221D6B, _STL::_Select1st<TreeHintPair00221D6B>, _STL::less<AsciiString>, _STL::allocator<TreeHintPair00221D6B> > TreeHint00221D6B;
+// Retail uses its static byte allocator and has no node cleanup catch block.
+namespace _STL {
+template <> class allocator<char> {
+public:
+    static char *allocate(unsigned int bytes, const void *hint);
+};
+}
+// ?_M_create_node@?$_Rb_tree@VAsciiString@@U?$pair@$$CBVAsciiString@@UTreeHintRef00221D6B@@@_STL@@U?$_Select1st@U?$pair@$$CBVAsciiString@@UTreeHintRef00221D6B@@@_STL@@@3@U?$less@VAsciiString@@@3@V?$allocator@U?$pair@$$CBVAsciiString@@UTreeHintRef00221D6B@@@_STL@@@3@@_STL@@IAEPAU?$_Rb_tree_node@U?$pair@$$CBVAsciiString@@UTreeHintRef00221D6B@@@_STL@@@2@ABU?$pair@$$CBVAsciiString@@UTreeHintRef00221D6B@@@2@@Z
+template <>
+TreeHint00221D6B::_Link_type TreeHint00221D6B::_M_create_node(const TreeHintPair00221D6B &value)
+{
+    _Link_type node = (_Link_type)_STL::allocator<char>::allocate(sizeof(_STL::_Rb_tree_node<TreeHintPair00221D6B>), 0);
+    _STL::_Construct(&node->_M_value_field, value);
+    return node;
+}
 template TreeHint00221D6B::iterator TreeHint00221D6B::insert_unique(TreeHint00221D6B::iterator, const TreeHintPair00221D6B &);
