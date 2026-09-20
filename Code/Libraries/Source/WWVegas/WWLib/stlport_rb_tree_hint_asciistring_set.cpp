5cf073f527044bc7d973569602e9f488b9ee8572
NotNBR <331651539+NotNBR@users.noreply.github.com>
2026-09-20 18:21:09 +0200

    Recover player faction-set clear (41 bytes)

--- Code/Libraries/Source/WWVegas/WWLib/stlport_rb_tree_hint_asciistring_set.cpp
+++ Code/Libraries/Source/WWVegas/WWLib/stlport_rb_tree_hint_asciistring_set.cpp
@@ -43,3 +43,4 @@ template StringSetTree::_Rb_tree(const StringSetTree &);
 // Faction-set cleanup: player callback22D920 -> tree dtor589BE -> clear57B4B.
 // Erase56CC3 destroys each string at node+16 through the proven dtor48BA39.
 template void StringSetTree::_M_erase(StringSetTree::_Link_type);
+template void StringSetTree::clear();
