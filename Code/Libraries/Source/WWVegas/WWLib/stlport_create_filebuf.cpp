82a4ec567c4228094bf0c2dfee3a7b8f5a74896a
NotNBR <331651539+NotNBR@users.noreply.github.com>
2026-09-20 18:35:28 +0200

    Match wide filebuf seekoff from STLport reference (920 bytes)

--- Code/Libraries/Source/WWVegas/WWLib/stlport_create_filebuf.cpp
+++ Code/Libraries/Source/WWVegas/WWLib/stlport_create_filebuf.cpp
@@ -1,111 +1,87 @@
-// cl: /O2 /EHsc /MD
-// stlport
-// STLport 4.5.3 _Stl_create_filebuf / _Stl_create_wfilebuf (src/iostream.cpp).
+/*
+ * Copyright (c) 1999
+ * Silicon Graphics Computer Systems, Inc.
+ *
+ * Copyright (c) 1999 
+ * Boris Fomitchev
+ *
+ * This material is provided "as is", with absolutely no warranty expressed
+ * or implied. Any use is at your own risk.
+ *
+ * Permission to use or copy this software for any purpose is hereby granted 
+ * without fee, provided the above notices are retained on all copies.
+ * Permission to modify the code and to distribute modified code is granted,
+ * provided the above notices are retained, and a notice that the code was
+ * modified is included with the above copyright notice.
+ *
+ */
+// STLport 4.5.3: reference X4Iostream.cpp global streams and filebuf factories.
+// Modified for BFME2: factories omit exception wrappers as in the matched retail.
+// Original stream context also emits the complete920B wide seekoff at149F0.
+// cl: /O2 /GR- /EHsc- /Ireference/open-bfme-1/vendor/stlport/src /Ireference/open-bfme-1/vendor/stlport /Ireference/open-bfme-1/vendor/stlport/stl /Ireference/open-bfme-1/vendor/stlport/using /Ireference/open-bfme-1/Code/stlport
+#include "stlport_prefix.h"
+#include <istream>
+#include <stl/_fstream.h>
+#include <stdio_streambuf>
+#include "aligned_buffer.h"
 
-struct _iobuf {
-	char *_ptr;
-	int _cnt;
-	char *_base;
-	int _flag;
-	int _file;
-	int _charbuf;
-	int _bufsiz;
-	char *_tmpfname;
-};
-typedef struct _iobuf FILE;
+#ifndef _STLP_USE_NAMESPACES
+# define cin _STLP_cin
+# define cout _STLP_cout
+# define cerr _STLP_cerr
+# define clog _STLP_clog
+#endif
 
-namespace _STL {
+_STLP_BEGIN_NAMESPACE
 
-class ios_base {
-public:
-	typedef int openmode;
-};
+#if defined (_STLP_MSVC) || defined(__MWERKS__) || defined (__ICL) || defined (__ISCPP__)
+# if defined(__MWERKS__)
+#  pragma suppress_init_code on
+# else
+#  pragma init_seg("STLPORT_NO_INIT")
+# endif
+_STLP_DECLSPEC istream cin(0);
+_STLP_DECLSPEC ostream cout(0);
+_STLP_DECLSPEC ostream cerr(0);
+_STLP_DECLSPEC ostream clog(0);
+_STLP_DECLSPEC wistream wcin(0);
+_STLP_DECLSPEC wostream wcout(0);
+_STLP_DECLSPEC wostream wcerr(0);
+_STLP_DECLSPEC wostream wclog(0);
+# if defined(__MWERKS__)
+#  pragma suppress_init_code off
+# endif
+#else
+_STLP_DECLSPEC _Stl_aligned_buffer<istream> cin;
+_STLP_DECLSPEC _Stl_aligned_buffer<ostream> cout;
+_STLP_DECLSPEC _Stl_aligned_buffer<ostream> cerr;
+_STLP_DECLSPEC _Stl_aligned_buffer<ostream> clog;
+# ifndef _STLP_NO_WCHAR_T
+_STLP_DECLSPEC _Stl_aligned_buffer<wistream> wcin;
+_STLP_DECLSPEC _Stl_aligned_buffer<wostream> wcout;
+_STLP_DECLSPEC _Stl_aligned_buffer<wostream> wcerr;
+_STLP_DECLSPEC _Stl_aligned_buffer<wostream> wclog;
+# endif
+#endif
 
-class _Filebuf_base {
-public:
-	bool _M_open(int file_no, int mode);
-	bool __is_open() const { return _M_is_open != 0; }
-
-private:
-	void *_M_file_id;
-	void *_M_view_id;
-	int _M_openmode;
-	unsigned char _M_is_open;
-	unsigned char _M_should_close;
-	unsigned char _M_regular_file;
-	unsigned char _M_pad;
-};
-
-inline int _FILE_fd(FILE *f) { return f->_file; }
-
-template <class CharT> class char_traits {};
-
-template <class CharT, class Traits>
-class basic_filebuf {};
-
-// Narrow: vptr + pad -> _M_base at +0x54; total size 0xB4.
-template <>
-class basic_filebuf<char, char_traits<char> > {
-public:
-	basic_filebuf();
-	virtual ~basic_filebuf();
-
-	bool is_open() const { return _M_base.__is_open(); }
-
-	basic_filebuf *_M_open(int fd, ios_base::openmode mode)
-	{
-		return _M_base._M_open(fd, mode) ? this : 0;
-	}
-
-private:
-	char m_streambuf[0x50];
-	_Filebuf_base _M_base;
-	char m_tail[0xB4 - 0x54 - sizeof(_Filebuf_base)];
-};
-
-// Wide (G): vptr + pad -> _M_base at +0x24; total size 0x8C.
-template <>
-class basic_filebuf<unsigned short, char_traits<unsigned short> > {
-public:
-	basic_filebuf();
-	virtual ~basic_filebuf();
-
-	bool is_open() const { return _M_base.__is_open(); }
-
-	basic_filebuf *_M_open(int fd, ios_base::openmode mode)
-	{
-		return _M_base._M_open(fd, mode) ? this : 0;
-	}
-
-private:
-	char m_streambuf[0x20];
-	_Filebuf_base _M_base;
-	char m_tail[0x8C - 0x24 - sizeof(_Filebuf_base)];
-};
-
-typedef basic_filebuf<char, char_traits<char> > filebuf;
-typedef basic_filebuf<unsigned short, char_traits<unsigned short> > wfilebuf;
-
-filebuf *_Stl_create_filebuf(FILE *f, ios_base::openmode mode)
+filebuf* _Stl_create_filebuf(FILE* f, ios_base::openmode mode)
 {
-	filebuf *result = new filebuf();
-	result->_M_open(_FILE_fd(f), mode);
-	if (!result->is_open()) {
-		delete result;
-		result = 0;
-	}
-	return result;
+  basic_filebuf<char, char_traits<char> >* result;
+  result = new basic_filebuf<char, char_traits<char> >();
+  result->_M_open(_FILE_fd(f), mode);
+  if (!result->is_open()) { delete result; result = 0; }
+  return result;
 }
 
-wfilebuf *_Stl_create_wfilebuf(FILE *f, ios_base::openmode mode)
+#ifndef _STLP_NO_WCHAR_T
+wfilebuf* _Stl_create_wfilebuf(FILE* f, ios_base::openmode mode)
 {
-	wfilebuf *result = new wfilebuf();
-	result->_M_open(_FILE_fd(f), mode);
-	if (!result->is_open()) {
-		delete result;
-		result = 0;
-	}
-	return result;
+  basic_filebuf<wchar_t, char_traits<wchar_t> >* result;
+  result = new basic_filebuf<wchar_t, char_traits<wchar_t> >();
+  result->_M_open(_FILE_fd(f), mode);
+  if (!result->is_open()) { delete result; result = 0; }
+  return result;
 }
+#endif
 
-} // namespace _STL
+_STLP_END_NAMESPACE
