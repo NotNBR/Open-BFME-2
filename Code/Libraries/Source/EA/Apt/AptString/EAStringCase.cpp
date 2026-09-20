9d4ce2e9b29d9ea926f66da16c77addedc5a7419
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-20 14:27:20 +0000

    Recover EAStringC::MakeLower 39B @0x006D5940 via BFME1 EAStringCase donor

---
+++ Code/Libraries/Source/EA/Apt/AptString/EAStringCase.cpp
@@ -0,0 +1,49 @@
+// cl: /O2 /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc
+
+extern "C" char *__cdecl _strlwr(char *);
+extern "C" char *__cdecl _strupr(char *);
+
+class EAStringC
+{
+	class StringDataC
+	{
+	public:
+		unsigned short m_uRefCount;
+		unsigned short m_uSize;
+		unsigned short m_uMaxSize;
+		unsigned short m_uHash;
+	};
+
+	class DebugDataC : public StringDataC
+	{
+		char m_strText[256];
+	};
+
+	enum CBPushZero
+	{
+		CB_NO_PUSH_ZERO,
+		CB_PUSH_ZERO
+	};
+
+	void ChangeBuffer(unsigned int uSizeToReserve, unsigned int uOffsetCopy,
+		unsigned int uSizeCopy, CBPushZero ePushZero, unsigned int uInternalSize);
+
+	char *GetInternalBuffer() const
+	{
+		return reinterpret_cast<char *>(m_pData) + sizeof(StringDataC);
+	}
+
+	DebugDataC *m_pData;
+
+public:
+	EAStringC &MakeLower();
+	EAStringC &MakeUpper(); // defined in the follow-up commit
+};
+
+EAStringC &EAStringC::MakeLower()
+{
+	const unsigned int size = m_pData->m_uSize;
+	ChangeBuffer(size, 0, size, CB_PUSH_ZERO, size);
+	_strlwr(GetInternalBuffer());
+	return *this;
+}
