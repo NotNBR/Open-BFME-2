b89cff00b3a6a71d3bc1e170714e6205f5f6ea2d
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-20 16:07:24 +0000

    Recover Lua _luaB_tostring 253B @0x0074BFA0 via BFME1 donor

---
+++ Code/Libraries/Source/Lua/luaB_tostring.c
@@ -0,0 +1,38 @@
+// cl: /MD /Ireference/open-bfme-1/Code/Libraries/Source/Lua
+// Lua's tostring binding uses EA's boolean tag value 6 in its type switch.
+
+#include <stdio.h>
+
+#include "lua.h"
+#include "lauxlib.h"
+
+int luaB_tostring(lua_State *L)
+{
+    char buff[64];
+
+    switch (lua_type(L, 1)) {
+    case LUA_TNUMBER:
+    case 6:
+        lua_pushstring(L, lua_tostring(L, 1));
+        return 1;
+    case LUA_TSTRING:
+        lua_pushvalue(L, 1);
+        return 1;
+    case LUA_TTABLE:
+        sprintf(buff, "table: %p", lua_topointer(L, 1));
+        break;
+    case LUA_TFUNCTION:
+        sprintf(buff, "function: %p", lua_topointer(L, 1));
+        break;
+    case LUA_TUSERDATA:
+        sprintf(buff, "userdata(%d): %p", lua_tag(L, 1), lua_touserdata(L, 1));
+        break;
+    case LUA_TNIL:
+        lua_pushstring(L, "nil");
+        return 1;
+    default:
+        luaL_argerror(L, 1, "value expected");
+    }
+    lua_pushstring(L, buff);
+    return 1;
+}
