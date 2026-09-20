ab796844706dc10688f6b8c4471c10f9100acbc1
NotNBR <331651539+NotNBR@users.noreply.github.com>
2026-09-20 18:46:46 +0200

    Match iostream global shutdown from reference (894 bytes)

--- Code/Libraries/Source/WWVegas/WWLib/stlport_create_filebuf.cpp
+++ Code/Libraries/Source/WWVegas/WWLib/stlport_create_filebuf.cpp
@@ -84,4 +84,57 @@ wfilebuf* _Stl_create_wfilebuf(FILE* f, ios_base::openmode mode)
 }
 #endif
 
+// BFME1 reference alias: BFME2 loc-init release70E0 uses the same
+// DDEB18 classic-locale object and DDEB20 counter as the held destructor.
+void rva00832100Release(void);
+
+long ios_base::Init::_S_count = 0;
+
+void _STLP_CALL ios_base::_S_uninitialize()
+{
+  if (Init::_S_count == 0)
+    return;
+
+  istream* ptr_cin  = __REINTERPRET_CAST(istream*,&cin);
+  ostream* ptr_cout = __REINTERPRET_CAST(ostream*,&cout);
+  ostream* ptr_cerr  = __REINTERPRET_CAST(ostream*,&cerr);
+  ostream* ptr_clog  = __REINTERPRET_CAST(ostream*,&clog);
+# ifndef _STLP_NO_WCHAR_T
+  wistream* ptr_wcin  = __REINTERPRET_CAST(wistream*,&wcin);
+  wostream* ptr_wcout = __REINTERPRET_CAST(wostream*,&wcout);
+  wostream* ptr_wcerr = __REINTERPRET_CAST(wostream*,&wcerr);
+  wostream* ptr_wclog = __REINTERPRET_CAST(wostream*,&wclog);
+# endif
+
+  ptr_cin->exceptions(0);
+  ptr_cout->exceptions(0);
+  ptr_cerr->exceptions(0);
+  ptr_clog->exceptions(0);
+  delete ptr_cin->rdbuf(0);
+  delete ptr_cout->rdbuf(0);
+  delete ptr_cerr->rdbuf(0);
+  delete ptr_clog->rdbuf(0);
+  _Destroy(ptr_cin);
+  _Destroy(ptr_cout);
+  _Destroy(ptr_cerr);
+  _Destroy(ptr_clog);
+# ifndef _STLP_NO_WCHAR_T
+  ptr_wcin->exceptions(0);
+  ptr_wcout->exceptions(0);
+  ptr_wcerr->exceptions(0);
+  ptr_wclog->exceptions(0);
+  delete ptr_wcin->rdbuf(0);
+  delete ptr_wcout->rdbuf(0);
+  delete ptr_wcerr->rdbuf(0);
+  delete ptr_wclog->rdbuf(0);
+  _Destroy(ptr_wcin);
+  _Destroy(ptr_wcout);
+  _Destroy(ptr_wcerr);
+  _Destroy(ptr_wclog);
+# endif
+  if (_Loc_init::_S_count > 0)
+    rva00832100Release();
+  --Init::_S_count;
+}
+
 _STLP_END_NAMESPACE
