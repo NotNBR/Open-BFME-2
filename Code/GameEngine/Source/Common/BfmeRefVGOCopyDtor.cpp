e490e1afda46a702129b0c4a1d648e610eb23063
Peppy Penguin <326628422+peppy-penguin@users.noreply.github.com>
2026-09-20 22:55:10 +0000

    Recover BfmeRefVGO dtor (0x000A9822, 30B) mirroring assign release half

--- Code/GameEngine/Source/Common/BfmeRefVGOCopyDtor.cpp
+++ Code/GameEngine/Source/Common/BfmeRefVGOCopyDtor.cpp
@@ -6,11 +6,14 @@
 // existing bfmeIncVGO pin at 0x006CFDE0.
 
 unsigned __cdecl bfmeIncVGO(unsigned *p);
+unsigned __cdecl bfmeDecVGO(unsigned *p);
+void __cdecl bfmeDropVGO(void *p);
 
 class BfmeRefVGO
 {
 public:
     BfmeRefVGO(const BfmeRefVGO &other);
+    ~BfmeRefVGO();
 
 private:
     unsigned *m_bfmeP;
@@ -22,4 +25,14 @@ BfmeRefVGO::BfmeRefVGO(const BfmeRefVGO &other)
     m_bfmeP = other.m_bfmeP;
     if (m_bfmeP)
         bfmeIncVGO(m_bfmeP);
+}
+
+// ??1BfmeRefVGO@@QAE@XZ, retail 0x000A9822 (30B). Mirror of the release half
+// of bfmeAssignVGO in BfmeConv1355.cpp: decrement, dropping the shared block
+// when the count reaches zero. Dec resolves through the rowed Rva006CFDF0DecRef
+// leaf, drop through the bfmeDropVGO pin at 0x006D0440.
+BfmeRefVGO::~BfmeRefVGO()
+{
+    if (m_bfmeP && bfmeDecVGO(m_bfmeP) == 0)
+        bfmeDropVGO(m_bfmeP);
 }
