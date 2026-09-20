df614a8d8829da5ddbbe82a3d9dd413e82cf3402
NotNBR <331651539+NotNBR@users.noreply.github.com>
2026-09-20 23:58:00 +0200

    Recover yres command handler

--- Code/GameEngine/Source/Common/CommandLineResolution.cpp
+++ Code/GameEngine/Source/Common/CommandLineResolution.cpp
@@ -16,3 +16,12 @@ int parseXRes(char *args[],int num) {
     }
     return 1;
 }
+
+// Live -yres table81FB78 identifies3B96C8 (45B); field is +34.
+int parseYRes(char *args[],int num) {
+    if(TheWritableGlobalData && num>1) {
+        TheWritableGlobalData->m_yResolution=atoi(args[1]);
+        return 2;
+    }
+    return 1;
+}
