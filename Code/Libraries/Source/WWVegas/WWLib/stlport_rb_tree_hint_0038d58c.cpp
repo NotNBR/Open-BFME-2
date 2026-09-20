fdcd0c650d22051d23a0520ec0764101bead5ba8
NotNBR <331651539+NotNBR@users.noreply.github.com>
2026-09-20 13:59:00 +0200

    Recover mapped-value tree 0038d58c node creation
    Use the BFME1/STLport insertion family with the independently verified BFME2 narrow-string comparator, 32-byte node layout, and key/pair-copy chain. The key copy is the established STLport basic_string<char> constructor0x9170; comparison calls the established lexicographical range helper0x24FC0. The mapped dword is copied at pair offset12; preserve unknown destruction via declarations.
    
    Full 34-byte body at 0x0038AAA8 matches after relocation resolution. No unresolved calls or masked aliases.

--- Code/Libraries/Source/WWVegas/WWLib/stlport_rb_tree_hint_0038d58c.cpp
+++ Code/Libraries/Source/WWVegas/WWLib/stlport_rb_tree_hint_0038d58c.cpp
@@ -18,3 +18,21 @@ typedef _STL::pair<const _STL::basic_string<char>, TreePayload0038D58C> TreePair
 typedef _STL::_Rb_tree<_STL::basic_string<char>, TreePair0038D58C, _STL::_Select1st<TreePair0038D58C>, _STL::less<_STL::basic_string<char> >, _STL::allocator<TreePair0038D58C> > Tree0038D58C;
 template Tree0038D58C::iterator Tree0038D58C::insert_unique(Tree0038D58C::iterator, const TreePair0038D58C &);
 
+namespace _STL { template <> class allocator<char> { public: static char *allocate(unsigned int bytes, const void *hint); static void deallocate(char *p, unsigned int bytes); }; }
+// ?_M_create_node@?$_Rb_tree@V?$basic_string@DV?$char_traits@D@_STL@@V?$allocator@D@2@@_STL@@U?$pair@$$CBV?$basic_string@DV?$char_traits@D@_STL@@V?$allocator@D@2@@_STL@@UTreePayload0038D58C@@@2@U?$_Select1st@U?$pair@$$CBV?$basic_string@DV?$char_traits@D@_STL@@V?$allocator@D@2@@_STL@@UTreePayload0038D58C@@@_STL@@@2@U?$less@V?$basic_string@DV?$char_traits@D@_STL@@V?$allocator@D@2@@_STL@@@2@V?$allocator@U?$pair@$$CBV?$basic_string@DV?$char_traits@D@_STL@@V?$allocator@D@2@@_STL@@UTreePayload0038D58C@@@_STL@@@2@@_STL@@IAEPAU?$_Rb_tree_node@U?$pair@$$CBV?$basic_string@DV?$char_traits@D@_STL@@V?$allocator@D@2@@_STL@@UTreePayload0038D58C@@@_STL@@@2@ABU?$pair@$$CBV?$basic_string@DV?$char_traits@D@_STL@@V?$allocator@D@2@@_STL@@UTreePayload0038D58C@@@2@@Z
+template <> Tree0038D58C::_Link_type Tree0038D58C::_M_create_node(const TreePair0038D58C &value)
+{
+    _Link_type node = (_Link_type)_STL::allocator<char>::allocate(sizeof(_STL::_Rb_tree_node<TreePair0038D58C>), 0);
+    _STL::_Construct(&node->_M_value_field, value);
+    return node;
+}
+
+
+
+
+
+
+
+
+
+
