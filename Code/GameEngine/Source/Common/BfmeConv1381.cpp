fb63ad3c8c32e321353aba60dfb7f3057394891e
muse-spark <muse-spark@local>
2026-09-20 13:44:19 +0000

    Recover bfmeGoVJB 174B @0x006566A0 via BFME1 BfmeConv1381 donor

--- Code/GameEngine/Source/Common/BfmeConv1381.cpp
+++ Code/GameEngine/Source/Common/BfmeConv1381.cpp
@@ -32,3 +32,21 @@ void __stdcall bfmeGoVJA(BfmeMsgVJA *m, void *name, void **kw, int n, int max)
 	m->bfmeSet3VJA("keywords.[]", n);
 }
 
+extern void *g_bfmeVJB;
+
+void __stdcall bfmeGoVJB(BfmeMsgVJA *m, void *name, void **kw, int n, int max)
+{
+	char buf[0x20];
+	void *g = g_bfmeVJB;
+	m->bfmeRunVJA();
+	m->m_bfme1c = 0x61636374;
+	m->bfmeSetVJA("TXN", g);
+	m->bfmeSetVJA("name", name);
+	m->bfmeSet3VJA("maxSuggestions", max);
+	for (int i = 0; i < n; ++i)
+	{
+		sprintf(buf, "keywords.%d", i);
+		m->bfmeSetVJA(buf, kw[i]);
+	}
+	m->bfmeSet3VJA("keywords.[]", n);
+}
